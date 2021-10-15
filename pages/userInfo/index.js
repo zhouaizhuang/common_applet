f1ca6b0986d6cb4fa00f78002ab10e52b0588dcd
周爱桩 <840043348@qq.com>
2021-10-15 16:17:51 +0800

    获取手机号代码

--- pages/userInfo/index.js
+++ pages/userInfo/index.js
@@ -1,36 +1,38 @@
 // pages/userInfo/index.js
+import { getUserProfile } from "../../api"
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    userInfo: {}, // 用户信息
-    hasUserInfo: false, // 用户信息是否存在
+    userInfo: '', // 用户信息
     canIUse: wx.canIUse('button.open-type.getUserInfo'), // 当前环境是否可以使用wx.getuserInfo
     canIUseGetUserProfile: false, // 当前环境是否可以使用wx.getUserProfile。默认是false
     canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName'), // 如需尝试获取用户信息可改为false
   },
-  getUserProfile(e) {
-  // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      lang: 'zh_CN',
-      success: (res) => {
-        console.log(res)
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    })
+  async getUserData(){
+    const userInfo = await getUserProfile()
+    this.setData({userInfo})
   },
   getUserInfo(e) {
     // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
+    this.setData({ userInfo: e.detail.userInfo })
+  },
+  // 目前该接口针对非个人开发者
+  // https://blog.csdn.net/chen_pan_pan/article/details/80606658
+  getPhoneNumber (e) {
     console.log(e)
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+    const { iv, encryptedData } = e.detail
+    console.log(iv)
+    wx.login({
+      success: res => {
+        const { code } = res 
+        // code用于获取用户的openID和sessionKey
+        // 后台根据参数【encryptedData】 、【iv】 、【sessionKey】解密获取用户手机号
+        console.log(iv, encryptedData, code)
+        
+      }
     })
   },
   /**
