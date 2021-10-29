fe7b0229f4d05a30c7ef0a2b986123170c0dd1f1
周爱桩 <840043348@qq.com>
2021-10-29 11:04:21 +0800

    增加图片预览

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,7 @@
 // index.js
 // 获取应用实例
 const app = getApp()
-import { navigateTo, navigateToMiniProgram, showLoading, previewImage } from "../../api"
+import { navigateTo, navigateToMiniProgram, showToast, previewImage, getImageInfo, saveImg2Album } from "../../api"
 Page({
   data: {
   },
@@ -11,8 +11,12 @@ Page({
   goOtherApplet(){
     navigateToMiniProgram({appId: 'wx26e3647f5b46bf93'})
   },
-  test(){
-    previewImage({urls:['http://tmp/rp35Y5PWoonk2ca2d1abcb9a7c0283e56df60f891ab4.png']})
+  async previewImg(){
+    previewImage({
+      urls:['https://upload.qianfanyun.com/yjyn/ylyn_index_bg_03.png', 'https://upload.qianfanyun.com/yjyn/ylyn_hat.png'],
+      showmenu: true, // 是否显示长按菜单
+      current: 'https://upload.qianfanyun.com/yjyn/ylyn_hat.png', // 设置默认显示的图片为第二张图片链接
+    })
   },
   async onLoad() {
   }
